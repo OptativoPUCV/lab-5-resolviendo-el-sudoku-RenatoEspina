d2f638d11be7edb12263f3197bb6229c4655d4e8
Your Name <you@example.com>
2025-05-16 17:12:20 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,8 +44,43 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
+ int visto[10];
 
-    return 1;
+    // Validar filas
+    for (int i = 0; i < 9; i++) {
+        for (int x = 0; x < 10; x++) visto[x] = 0;
+        for (int j = 0; j < 9; j++) {
+            int val = n->sudo[i][j];
+            if (val == 0) continue;
+            if (visto[val]) return 0;
+            visto[val] = 1;
+        }
+    }
+
+    // Validar columnas
+    for (int j = 0; j < 9; j++) {
+        for (int x = 0; x < 10; x++) visto[x] = 0;
+        for (int i = 0; i < 9; i++) {
+            int val = n->sudo[i][j];
+            if (val == 0) continue;
+            if (visto[val]) return 0;
+            visto[val] = 1;
+        }
+    }
+
+    // Validar submatrices 3x3
+  for (int k = 0; k < 9; k++) {
+    for (int x = 0; x < 10; x++) visto[x] = 0;
+    for (int p = 0; p < 9; p++) {
+      int i = 3 * (k / 3) + (p / 3);
+      int j = 3 * (k % 3) + (p % 3);
+      int val = n->sudo[i][j];
+      if (val == 0) continue;
+      if (visto[val]) return 0;
+      visto[val] = 1;
+    }
+  }
+  return 1;
 }
 
 
